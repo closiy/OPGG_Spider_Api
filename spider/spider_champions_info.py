95881d7fde42ecc98574dd634b1160e1a2bdbfee
closiy <closiycai@gmail.com>
2019-04-09 17:30:08 +0800

    完成英雄数据上顶栏的数据爬取

--- spider/spider_champions_info.py
+++ spider/spider_champions_info.py
@@ -3,47 +3,93 @@ import chardet
 import pymongo
 from bs4 import BeautifulSoup
 
-# connect to mongodb
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-mydb = myclient['demacia_db']
-mycol = mydb['champions']
 
 
-url = 'https://www.op.gg/champion/statistics'
-r1 = requests.get(url)
-r1.encoding = chardet.detect(r1.content)["encoding"]
-soup = BeautifulSoup(r1.text, 'html.parser')
+def get_champions_data(url, data_champion_key ):
+    '''
+    :param url:
+    :param data_champion_key:
+    :return:
+    '''
+    r2 = requests.get(url)
+    r2.encoding = chardet.detect(r2.content)["encoding"]
+    soup2 = BeautifulSoup(r2.text, 'html.parser')
+    '''
+    表头数据处理
+    '''
+    header = soup2.find_all(name='div', attrs=['class', 'l-champion-statistics-header'])
+    print(header)
+
+    '''
+    tabWrap _recognized
+    表中数据处理
+    
+    '''
+
+def pos_changeto_id(pos):
+
+    '''
+    change position text to url position id
+    :param pos:
+    :return:
+    '''
+
+    if pos == 'Middle':
+        return 'mid'
+    if pos == 'Jungle':
+        return 'jungle'
+    if pos == 'Support':
+        return 'support'
+    if pos == 'Top':
+        return 'top'
+    if pos == 'Bottom':
+        return 'bot'
 
 
-# 获取英雄的 名称 和 对应爬取网址
-for content in soup.find_all(name='div', class_='champion-index__champion-list'):
-    for champions in content.find_all(name='div', attrs={'data-champion-key': True}):
-        # 打印 关键属性 data-champion-key
+if __name__ == '__main__':
+    # 获取英雄的 名称 和 对应爬取网址
+    # connect to mongodb
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myclient['demacia_db']
+    mycol = mydb['champions']
+    url = 'https://www.op.gg/champion/statistics'
+    r1 = requests.get(url)
+    r1.encoding = chardet.detect(r1.content)["encoding"]
+    soup = BeautifulSoup(r1.text, 'html.parser')
+    for content in soup.find_all(name='div', class_='champion-index__champion-list'):
+        for champions in content.find_all(name='div', attrs={'data-champion-key': True}):
+            # 打印 关键属性 data-champion-key
 
-        # print(champions.attrs['data-champion-key'], end=" ")
-        a_list = champions.find_all(name='a', attrs={'href':True})
-        for a in a_list:
-            url = 'https://www.op.gg' + a.attrs['href']
-            # save datas to Collection in champions of demacia_db in localhost
+            # print(champions.attrs['data-champion-key'], end=" ")
+            a_list = champions.find_all(name='a', attrs={'href':True})
+            for a in a_list:
+                url = 'https://www.op.gg' + a.attrs['href']
+                # save datas to Collection in champions of demacia_db in localhost
+                champion_key = {'data-champion-key': champions.attrs['data-champion-key']}  # champion ID
+                a_pos = a.find_all(name='div', class_='champion-index__champion-item__position')
+                print(champion_key)
+                for a_pos_i in a_pos:
+                    pos = a_pos_i.find(name='span')
+                    pos_url = url + '/' + pos_changeto_id(pos.text)
+                    champion_pos_text = {'data-champion-pos-text': pos.text}  # champion position names such as Middle
+                    champion_pos_id = {'data-champion-pos-id': pos_changeto_id(pos.text)}  # champion position ID such as mid
+                    champion_pos_url = {'data-champion-pos-url': pos_url}
+                    '''
+                    插入数据
+                    insert data
+                    '''
+                    mydict = {'data-champion-key': champions.attrs['data-champion-key'],
+                              'data-champion-pos-text': champion_pos_text['data-champion-pos-text'],
+                              'data-champion-pos-id': champion_pos_id['data-champion-pos-id'],
+                              'data-champion-pos-url': champion_pos_url['data-champion-pos-url'],
+                              }
+                    mycol.insert_one(mydict)
+                    print('已插入', mydict)
+                # if mypos:
+                #     print('存在！')
+                # else:
+                #     print('不存在')
 
-            champion_key = {'data-champion-key': champions.attrs['data-champion-key']}
-            mydoc = mycol.find_one(champion_key)
-            if mydoc:
-                '''
-                更新数据
-                Update data
-                '''
-                old_query = {'url': mydoc['url']}
-                update_query = {'$set': {'url': url}}
-                print('已更新', end=" ")
-                print(mydoc)
-            else:
-                '''
-                插入数据
-                insert data
-                '''
-                mydict = {'data-champion-key': champions.attrs['data-champion-key'], 'url': url}
-                #mycol.insert_one(mydict)
-                print('已插入', mydict)
 
-        # 将获取的数据 存入数据库 或者 保存为 .txt or .json 文件
+
+            # 将获取的数据 存入数据库 或者 保存为 .txt or .json 文件
--- test.py
+++ test.py
@@ -1,18 +1,91 @@
 #!/usr/bin/python3
+import requests
+import chardet
 import pymongo
+from bs4 import BeautifulSoup
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-mydb = myclient['demacia_db']
-mycol = mydb['champions']
-# mydict = {'name':'closiy', 'alexa':'10000', 'url':'https://baidu.com'}
+# split url to get counter champions id
+def get_champion_id_from_url(data_champion_counter_url):
+    return data_champion_counter_url.split('/')[-1].split('.')[0].lower()
 
-champion_key = {'data-champion-key': 'yorick'}
-new_url = 'https://www.op.gg/champion/yorick/statistics'
-mydoc = mycol.find_one(champion_key)
-if mydoc:
-    old_query = {'url': mydoc['url']}
-    update_query = {'$set': {'url': new_url}}
-    mycol.update_one(old_query, update_query)
-    print(mydoc)
-else:
-    print("not exit")
+def get_champions_data(data_champion_pos_url, data_champion_key, mycol):
+    '''
+    get champion data in it's position
+    :param data_champion_pos_url:
+    :param data_champion_key:
+    :return:
+    '''
+    r2 = requests.get(data_champion_pos_url)
+    r2.encoding = chardet.detect(r2.content)["encoding"]
+    soup2 = BeautifulSoup(r2.text, 'html.parser')
+    champion_flag = {'data-champion-pos-url': data_champion_pos_url}  # flag of champion, to find out data
+
+    '''
+    get header info from OPGG champions page
+    '''
+    # header of pages, data-type as  position_role position_rate
+    header = soup2.find(name='div', attrs=['class', 'l-champion-statistics-header'])
+    li_position = header.find(name='li', class_= 'champion-stats-header__position champion-stats-header__position--active')
+
+    # get and update information of rate
+    rate_span = li_position.find(name='span', attrs=['class', 'champion-stats-header__position__rate'])
+    champion_pos_rate = {'data-champion-pos-rate': rate_span.text}
+    mycol.update_one(champion_flag, {'$set': {'data-champion-pos-rate': champion_pos_rate['data-champion-pos-rate']}})
+
+    # get and update img of champion
+    div_img = header.find(name='div', class_='champion-stats-header-info__image')
+    champion_img = div_img.find(name='img')
+    data_champion_img_url = 'https:' + champion_img.attrs['src']
+    mycol.update_one(champion_flag, {'$set': {'data-champion-img-url': data_champion_img_url}})
+
+    # get and update tier of champion
+    div_tier = header.find(name='div', class_='champion-stats-header-info__tier')
+    champion_tier = div_tier.find(name='b')
+    data_champion_tier = champion_tier.text
+    mycol.update_one(champion_flag, {'$set': {'data-champion-tier': data_champion_tier}})
+
+    # get and update counter champion
+    table_counter = header.find(name='table', class_='champion-stats-header-matchup__table champion-stats-header-matchup__table--strong tabItem')
+    data_champion_counter_list = []
+    i = 1
+    for tr_counter in table_counter.find_all(name='tr'):
+        data_champion_counter_url = 'https:' + tr_counter.find(name='img')['src']
+        data_champion_counter_winrate = tr_counter.find(name='b').text
+        data_champion_counter_id = get_champion_id_from_url(data_champion_counter_url)
+        data_champion_counter_list += [{
+            'data-champion-counter-id' + str(i): data_champion_counter_id,
+            'data-champion-counter-url-' + str(i): data_champion_counter_url,
+            'data-champion-counter-winrate-' + str(i): data_champion_counter_winrate}]
+        i += 1
+    mycol.update_one(champion_flag, {'$set': {'data-champion-counter-list': data_champion_counter_list}})
+
+    # get and update anti-counter champion
+    table_anticounter = header.find(name='table',
+                                class_='champion-stats-header-matchup__table champion-stats-header-matchup__table--weak tabItem')
+    data_champion_anticounter_list = []
+    i = 1
+    for tr_anticounter in table_anticounter.find_all(name='tr'):
+        data_champion_anticounter_url = 'https:' + tr_anticounter.find(name='img')['src']
+        data_champion_anticounter_winrate = tr_anticounter.find(name='b').text
+        data_champion_anticounter_id = get_champion_id_from_url(data_champion_anticounter_url)
+        data_champion_anticounter_list += [{
+            'data-champion-anticounter-id' + str(i): data_champion_anticounter_id,
+            'data-champion-anticounter-url-' + str(i): data_champion_anticounter_url,
+            'data-champion-anticounter-winrate-' + str(i): data_champion_anticounter_winrate}]
+        i += 1
+    mycol.update_one(champion_flag, {'$set': {'data-champion-anticounter-list': data_champion_anticounter_list}})
+    # output data
+    print(mycol.find_one(champion_flag))
+
+
+
+if __name__ == '__main__':
+    # connect to mongodb
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myclient['demacia_db']
+    mycol = mydb['champions']
+
+    # test data of 'neeko'
+    data_champion_key = 'neeko'
+    data_champion_pos_url = 'https://www.op.gg/champion/neeko/statistics/mid'
+    get_champions_data(data_champion_pos_url, data_champion_key, mycol)
